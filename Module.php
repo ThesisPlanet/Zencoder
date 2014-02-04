d83cdc462b89fc9d4728995956cb658c96dbbf0c
Jack Peterson <jack.peterson@gmail.com>
2014-02-04 08:40:28 -0800

    fix autoload issue

--- Module.php
+++ Module.php
@@ -1,31 +1,7 @@
 <?php
-namespace Zencoder;
-
-class Module
-{
-
-    public function getAutoloaderConfig ()
-    {
-        return array(
-                'Zend\Loader\ClassMapAutoloader' => array(
-                        __DIR__ . '/autoload_classmap.php'
-                ),
-                'Zend\Loader\StandardAutoloader' => array(
-                        'namespaces' => array(
-                                __NAMESPACE__ => __DIR__ . '/src/' .
-                                         __NAMESPACE__
-                        )
-                )
-        );
-    }
-
-    public function getConfig ()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function getServiceConfig ()
-    {
-        return array();
-    }
-}
+/**
+ * This file is placed here for compatibility with Zendframework 2's ModuleManager.
+ * It allows usage of this module even without composer.
+ * The original Module.php is in 'src/Zencoder' in order to respect PSR-0
+ */
+require_once __DIR__ . '/src/Zencoder/Module.php';
